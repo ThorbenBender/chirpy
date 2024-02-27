bafe5c1d4fa5986500cae58b8e17bc630cd31438
Thorben Bender <bender.thorben0@gmail.com>
2024-02-27 21:36:15 +0100

    Go client

---
+++ api_config.go
@@ -0,0 +1,37 @@
+package main
+
+
+import (
+  "net/http"
+  "fmt"
+)
+
+type apiConfig struct {
+  fileServerHits int
+}
+
+func (cfg *apiConfig) handleMetrics(w http.ResponseWriter, r *http.Request) {
+  w.Header().Add("Content-Type", "text/html; charset=utf-8")
+  w.WriteHeader(http.StatusOK)
+  w.Write([]byte(fmt.Sprintf(`
+  <html>
+    <body>
+      <h1>Welcome, Chirpy Admin</h1>
+      <p>Chirpy has been visited %d times!</p>
+    </body>
+  </html>
+  `,cfg.fileServerHits)))
+}
+
+func (cfg *apiConfig) handleReset(w http.ResponseWriter, r *http.Request) {
+  cfg.fileServerHits = 0
+  w.WriteHeader(http.StatusOK)
+  w.Write([]byte("Hits reset to 0"))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+  return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+    cfg.fileServerHits += 1
+    next.ServeHTTP(w, r)
+  })
+}
---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+	"net/http"
+  "github.com/go-chi/chi/v5"
+)
+
+func main() {
+  const filePathRoot = "."
+  const port = "8080"
+  apiCfg := apiConfig{
+    fileServerHits: 0,
+  } 
+  router := chi.NewRouter()
+  fsHandler := apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoot))))
+  router.Handle("/app/*", fsHandler)
+  router.Handle("/app", fsHandler)
+  
+  apiRouter := chi.NewRouter()
+  apiRouter.Get("/healthz", handleReadiness)
+  apiRouter.HandleFunc("/reset", apiCfg.handleReset)
+
+  adminRouter := chi.NewRouter()
+  adminRouter.Get("/metrics", apiCfg.handleMetrics)
+
+
+
+  router.Mount("/api", apiRouter)
+  router.Mount("/admin", adminRouter)
+  corsMux := middlewareCors(router)
+  server := &http.Server{
+    Addr: ":" + port,
+    Handler: corsMux,
+  }
+  log.Printf("Serving on port: %s\n", port)
+  log.Fatal(server.ListenAndServe())
+}
